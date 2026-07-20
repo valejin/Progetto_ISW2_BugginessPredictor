154bb5be703c93e755ab21f1353b8d7f4e9a95af
valejin <valentina.jin@students.uniroma2.eu>
2026-07-20 22:40:17 +0200

    checkpoint inventario intermedio (Project, Release, ClassName, Bugginess)

---
+++ src/main/java/it/uniroma2/isw2/dataset/DatasetCsvWriter.java
@@ -0,0 +1,28 @@
+package it.uniroma2.isw2.dataset;
+
+import it.uniroma2.isw2.model.ReleaseClassEntry;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class DatasetCsvWriter {
+
+    public void writeLabeledInventory(String outputPath,
+                                      String projectName,
+                                      List<ReleaseClassEntry> inventory,
+                                      Map<String, Boolean> labels) throws IOException {
+        try (FileWriter writer = new FileWriter(outputPath)) {
+            writer.append("Project,Release,ClassName,Bugginess\n");
+            for (ReleaseClassEntry rc : inventory) {
+                String key = rc.getReleaseId() + "#" + rc.getClassPath();
+                boolean buggy = labels.getOrDefault(key, false);
+                writer.append(projectName).append(",")
+                        .append(String.valueOf(rc.getReleaseId())).append(",")
+                        .append("\"").append(rc.getClassPath()).append("\",")
+                        .append(buggy ? "Yes" : "No").append("\n");
+            }
+        }
+    }
+}
